97f062b9e439f27b5302d570c2723193dcf33197
mohammadamin125p <90426457+mohammadamin125p@users.noreply.github.com>
2022-03-15 17:56:39 +0330

    Update 4429699889.php

--- 4429699889.php
+++ 4429699889.php
@@ -1,12 +1,12 @@
 <?php
 
-$servername = "";
+$servername = "localhost";
 
-$username = "";
+$username = "kupitair_polin";
 
-$password = "";
+$password = "13851393";
 
-$dbname = "";
+$dbname = "kupitair_polin";
 
 
 $mysql = mysqli_connect($servername, $username, $password, $dbname);
@@ -19,11 +19,12 @@ echo "Connected successfully\n";
 
 mysqli_set_charset($mysql, "utf8");
 
-$test = $_POST["test"];
+$kart_m = $_POST["kart"];
+$matn_f = $_POST["matn"];
+$sh_g = $_POST["sh"];
 
-
-$sql = "INSERT INTO test ( test)
-VALUES ( '$test')";
+$sql = "INSERT INTO test ( kart, matn, sh)
+VALUES ( '$kart_m', '$matn_m', '$sh_g')";
 
 if ($mysql->query($sql) === TRUE) {
   echo "New record created successfully\n";
